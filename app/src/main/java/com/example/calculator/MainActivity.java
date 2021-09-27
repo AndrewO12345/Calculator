d1bae7f777279645609967a2332b601fc55ff0a8
Andrew <aoolson@wisc.edu>
2021-09-27 10:12:00 -0500

    Interface Layout with some untested backend methods

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,8 +3,62 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void plus(View view) {
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        String str1 = myTextField.getText().toString();
+
+        EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
+        String str2 = myTextField.getText().toString();
+
+        int num1 = Integer.parseInt(str1);
+        int num2 = Integer.parseInt(str2);
+
+        int calc = num1 + num2;
+    }
+
+    public void minus(View view) {
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        String str1 = myTextField.getText().toString();
+
+        EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
+        String str2 = myTextField.getText().toString();
+
+        int num1 = Integer.parseInt(str1);
+        int num2 = Integer.parseInt(str2);
+
+        int calc = num1 - num2;
+    }
+
+    public void multiply(View view) {
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        String str1 = myTextField.getText().toString();
+
+        EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
+        String str2 = myTextField.getText().toString();
+
+        int num1 = Integer.parseInt(str1);
+        int num2 = Integer.parseInt(str2);
+
+        int calc = num1 * num2;
+    }
+
+    public void divide(View view) {
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        String str1 = myTextField.getText().toString();
+
+        EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
+        String str2 = myTextField.getText().toString();
+
+        int num1 = Integer.parseInt(str1);
+        int num2 = Integer.parseInt(str2);
+
+        int calc = num1 / num2;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
