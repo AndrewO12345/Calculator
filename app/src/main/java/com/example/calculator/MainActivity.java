3f6410548da28105e23eff39415866729169fbdf
Andrew <aoolson@wisc.edu>
2021-09-27 10:25:21 -0500

    Working calculator with new display activity

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -13,12 +14,15 @@ public class MainActivity extends AppCompatActivity {
         String str1 = myTextField.getText().toString();
 
         EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
-        String str2 = myTextField.getText().toString();
+        String str2 = myTextField2.getText().toString();
 
         int num1 = Integer.parseInt(str1);
         int num2 = Integer.parseInt(str2);
 
         int calc = num1 + num2;
+
+        String ret = Integer.toString(calc);
+        goToActivity(ret);
     }
 
     public void minus(View view) {
@@ -26,12 +30,15 @@ public class MainActivity extends AppCompatActivity {
         String str1 = myTextField.getText().toString();
 
         EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
-        String str2 = myTextField.getText().toString();
+        String str2 = myTextField2.getText().toString();
 
         int num1 = Integer.parseInt(str1);
         int num2 = Integer.parseInt(str2);
 
         int calc = num1 - num2;
+
+        String ret = Integer.toString(calc);
+        goToActivity(ret);
     }
 
     public void multiply(View view) {
@@ -39,12 +46,15 @@ public class MainActivity extends AppCompatActivity {
         String str1 = myTextField.getText().toString();
 
         EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
-        String str2 = myTextField.getText().toString();
+        String str2 = myTextField2.getText().toString();
 
         int num1 = Integer.parseInt(str1);
         int num2 = Integer.parseInt(str2);
 
         int calc = num1 * num2;
+
+        String ret = Integer.toString(calc);
+        goToActivity(ret);
     }
 
     public void divide(View view) {
@@ -52,12 +62,21 @@ public class MainActivity extends AppCompatActivity {
         String str1 = myTextField.getText().toString();
 
         EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
-        String str2 = myTextField.getText().toString();
+        String str2 = myTextField2.getText().toString();
 
         int num1 = Integer.parseInt(str1);
         int num2 = Integer.parseInt(str2);
 
         int calc = num1 / num2;
+
+        String ret = Integer.toString(calc);
+        goToActivity(ret);
+    }
+
+    public void goToActivity(String s) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("message", s);
+        startActivity(intent);
     }
 
     @Override
